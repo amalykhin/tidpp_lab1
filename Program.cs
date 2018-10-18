a738d260013f5b50b639bac78308a2028aeefe3b
Malykhin Andrey <andrei.malihin@ati.utm.md>
2018-10-18 11:57:58 +0300

    Revert "Not input is provided as function paraeters."
    This reverts commit a73c377d4f9b1afc048af6883114e404b756b845.

--- Program.cs
+++ Program.cs
@@ -4,23 +4,22 @@ namespace lab1
 {
     class Program
     {
+        static double a, b, c;
         static void Main(string[] args)
         {
-            double a, b, c;
-
             Console.WriteLine("Enter quadratic equation coefficients:");
             if (
                 double.TryParse(Console.ReadLine(), out a) &&
                 double.TryParse(Console.ReadLine(), out b) &&
                 double.TryParse(Console.ReadLine(), out c) 
             ) {
-                Console.WriteLine("x = [{0}]", string.Join(", ", Calculate(a, b, c)));
+                Console.WriteLine("x = [{0}]", string.Join(", ", Calculate()));
             } else {
                 Console.WriteLine("Input parse failed.");
             }
         }
 
-        static double[] Calculate(double a, double b, double c) {
+        static double[] Calculate() {
             double discrRoot = Math.Sqrt(b*b - 4*a*c);
             return new double[] {(-b-discrRoot)/(2*a), (-b+discrRoot)/(2*a)};
         }
