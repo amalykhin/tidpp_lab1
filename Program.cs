063523f59df242081daaf18a7b462b85da6fbade
Malykhin Andrey <andrei.malihin@ati.utm.md>
2018-10-18 12:04:31 +0300

    Added input parameters to Calculate().

--- Program.cs
+++ Program.cs
@@ -4,22 +4,22 @@ namespace lab1
 {
     class Program
     {
-        static double a, b, c;
         static void Main(string[] args)
         {
+            double a, b, c;
             Console.WriteLine("Enter quadratic equation coefficients:");
             if (
                 double.TryParse(Console.ReadLine(), out a) &&
                 double.TryParse(Console.ReadLine(), out b) &&
                 double.TryParse(Console.ReadLine(), out c) 
             ) {
-                Console.WriteLine("x = [{0}]", string.Join(", ", Calculate()));
+                Console.WriteLine("x = [{0}]", string.Join(", ", Calculate(a, b, c)));
             } else {
                 Console.WriteLine("Input parse failed.");
             }
         }
 
-        static double[] Calculate() {
+        static double[] Calculate(double a1, double a2, double a3) {
             double discrRoot = Math.Sqrt(b*b - 4*a*c);
             return new double[] {(-b-discrRoot)/(2*a), (-b+discrRoot)/(2*a)};
         }
